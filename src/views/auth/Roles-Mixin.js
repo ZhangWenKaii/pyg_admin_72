e7ba41616c1698d930b39d1154d31696e8241a71
zhousg <zhoushugang@itcast.cn>
2019-06-11 17:40:06 +0800

    添加&编辑&删除

--- src/views/auth/Roles-Mixin.js
+++ src/views/auth/Roles-Mixin.js
@@ -1,13 +1,100 @@
 export default {
   data() {
     return {
-      rolesList: []
+      rolesList: [],
+      // 添加角色相关数据
+      addDialogFormVisible: false,
+      addForm: {
+        roleName: "",
+        roleDesc: ""
+      },
+      rules: {
+        roleName: [
+          { required: true, message: "角色名称必填", trigger: "blur" }
+        ],
+        roleDesc: [{ required: true, message: "角色描述必填", trigger: "blur" }]
+      },
+      // 编辑相关的数据
+      editDialogFormVisible: false,
+      editForm: {
+        roleName: "",
+        roleDesc: ""
+      }
     };
   },
   created() {
     this.getData();
   },
   methods: {
+    // 打开编辑角色对话框
+    openEditDialog(row) {
+      this.editDialogFormVisible = true;
+      this.$nextTick(() => {
+        // 重置表单（数据 样式）
+        this.$refs.editForm.resetFields();
+        // 填充数据 row
+        // 将来提交请求需要ID
+        this.editForm.id = row.id;
+        this.editForm.roleName = row.roleName;
+        this.editForm.roleDesc = row.roleDesc;
+      });
+    },
+    // 编辑请求
+    editRole() {
+      this.$refs.editForm.validate(async valid => {
+        if (valid) {
+          const {
+            data: { meta }
+          } = await this.$http.put(`roles/${this.editForm.id}`, {
+            roleName: this.editForm.roleName,
+            roleDesc: this.editForm.roleDesc
+          });
+          if (meta.status !== 200) return this.$message.error("编辑角色失败");
+          // 成功
+          this.editDialogFormVisible = false;
+          this.getData();
+        }
+      });
+    },
+    // 删除业务
+    delRole(roleId) {
+      this.$confirm("亲，是否删除该角色?", "温馨提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          const {
+            data: { meta }
+          } = await this.$http.delete(`roles/${roleId}`);
+          if (meta.status !== 200) return this.$message.error("删除角色失败");
+          this.getData();
+        })
+        .catch(() => null);
+    },
+    // 打开添加对话框
+    openAddDialog() {
+      this.addDialogFormVisible = true;
+      this.$nextTick(() => {
+        this.$refs.addForm.resetFields();
+      });
+    },
+    // 添加请求
+    addRole() {
+      // 1. 校验整个表单
+      this.$refs.addForm.validate(async valid => {
+        if (valid) {
+          // 2. 知道提交的数据有哪些？roleName roleDesc ---> addForm
+          const {
+            data: { meta }
+          } = await this.$http.post("/roles", this.addForm);
+          if (meta.status !== 201) return this.$message.error("添加角色失败");
+          // 3. 成功
+          this.getData();
+          this.addDialogFormVisible = false;
+        }
+      });
+    },
     async getData() {
       // 获取角色列表数据
       const {
--- src/views/auth/Roles.vue
+++ src/views/auth/Roles.vue
@@ -3,47 +3,54 @@
     <!-- 自定义面包屑组件 -->
     <my-bread :list="['权限管理', '角色列表']"></my-bread>
     <el-card class="box-card">
-      <el-button type="primary">添加角色</el-button>
+      <el-button type="primary" @click="openAddDialog()">添加角色</el-button>
       <el-table :data="rolesList" border style="width:100%">
         <el-table-column type="expand">
           <template slot-scope="scope">
             <!-- 权限数据  需要布局 -->
-            <el-row
-              style="border-bottom:1px solid #eee"
-              :style="{ 'border-top': index === 0 ? '1px solid #eee' : 'none' }"
-              :key="item.id"
-              v-for="(item, index) in scope.row.children"
-            >
-              <el-col :span="4">
-                <el-tag closable>{{ item.authName }} </el-tag>
-                <i class="el-icon-caret-right"></i>
-              </el-col>
-              <el-col :span="20">
-                <el-row
-                  :key="subItem.id"
-                  v-for="(subItem, subIndex) in item.children"
-                  :style="{
-                    'border-top': subIndex !== 0 ? '1px solid #eee' : 'none'
-                  }"
-                >
-                  <el-col :span="6">
-                    <el-tag closable type="success"
-                      >{{ subItem.authName }}
-                    </el-tag>
-                    <i class="el-icon-caret-right"></i>
-                  </el-col>
-                  <el-col :span="18">
-                    <el-tag
-                      :key="lastItem.id"
-                      v-for="lastItem in subItem.children"
-                      closable
-                      type="warning"
-                      >{{ lastItem.authName }}
-                    </el-tag>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
+            <div v-if="!scope.row.children.length" style="color:#ccc">
+              该角色未分配任何权限
+            </div>
+            <div v-if="scope.row.children.length">
+              <el-row
+                style="border-bottom:1px solid #eee"
+                :style="{
+                  'border-top': index === 0 ? '1px solid #eee' : 'none'
+                }"
+                :key="item.id"
+                v-for="(item, index) in scope.row.children"
+              >
+                <el-col :span="4">
+                  <el-tag closable>{{ item.authName }} </el-tag>
+                  <i class="el-icon-caret-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-row
+                    :key="subItem.id"
+                    v-for="(subItem, subIndex) in item.children"
+                    :style="{
+                      'border-top': subIndex !== 0 ? '1px solid #eee' : 'none'
+                    }"
+                  >
+                    <el-col :span="6">
+                      <el-tag closable type="success"
+                        >{{ subItem.authName }}
+                      </el-tag>
+                      <i class="el-icon-caret-right"></i>
+                    </el-col>
+                    <el-col :span="18">
+                      <el-tag
+                        :key="lastItem.id"
+                        v-for="lastItem in subItem.children"
+                        closable
+                        type="warning"
+                        >{{ lastItem.authName }}
+                      </el-tag>
+                    </el-col>
+                  </el-row>
+                </el-col>
+              </el-row>
+            </div>
           </template>
         </el-table-column>
         <el-table-column type="index"></el-table-column>
@@ -52,14 +59,64 @@
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button-group>
-              <el-button icon="el-icon-edit" type="primary"></el-button>
-              <el-button icon="el-icon-delete" type="primary"></el-button>
+              <el-button
+                @click="openEditDialog(scope.row)"
+                icon="el-icon-edit"
+                type="primary"
+              ></el-button>
+              <el-button
+                icon="el-icon-delete"
+                @click="delRole(scope.row.id)"
+                type="primary"
+              ></el-button>
               <el-button icon="el-icon-setting" type="primary"></el-button>
             </el-button-group>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
+    <!-- 添加角色对话框 -->
+    <el-dialog title="添加角色" :visible.sync="addDialogFormVisible">
+      <el-form
+        :rules="rules"
+        :model="addForm"
+        label-width="80px"
+        autocomplete="off"
+        ref="addForm"
+      >
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="addForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="addForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addRole()">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑角色对话框 -->
+    <el-dialog title="编辑角色" :visible.sync="editDialogFormVisible">
+      <el-form
+        :rules="rules"
+        :model="editForm"
+        label-width="80px"
+        autocomplete="off"
+        ref="editForm"
+      >
+        <el-form-item label="角色名称" prop="roleName">
+          <el-input v-model="editForm.roleName"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" prop="roleDesc">
+          <el-input v-model="editForm.roleDesc"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editRole()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
