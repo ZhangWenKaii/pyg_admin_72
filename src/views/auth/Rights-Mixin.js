87fcb3370d7ff507e129cd9e1487f01f006fdd11
zhousg <zhoushugang@itcast.cn>
2019-06-11 14:55:38 +0800

    权限管理-权限列表-渲染

--- src/views/auth/Rights-Mixin.js
+++ src/views/auth/Rights-Mixin.js
@@ -1,9 +1,19 @@
 export default {
-  created() {},
+  created() {
+    this.getData();
+  },
   data() {
     return {
       rightsList: []
     };
   },
-  methods: {}
+  methods: {
+    async getData() {
+      const {
+        data: { data, meta }
+      } = await this.$http.get("rights/list");
+      if (meta.status !== 200) return this.$message.error("获取权限数据失败");
+      this.rightsList = data;
+    }
+  }
 };
--- src/views/auth/Rights.vue
+++ src/views/auth/Rights.vue
@@ -10,9 +10,19 @@
       <el-table :data="rightsList" height="400" border style="width: 100%">
         <!-- 序号列 -->
         <el-table-column type="index"></el-table-column>
-        <el-table-column label="权限名称" prop=""></el-table-column>
-        <el-table-column label="路径" prop=""></el-table-column>
-        <el-table-column label="权限等级" prop=""></el-table-column>
+        <el-table-column label="权限名称" prop="authName"></el-table-column>
+        <el-table-column label="路径" prop="path"></el-table-column>
+        <el-table-column label="权限等级" prop="level">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级权限</el-tag>
+            <el-tag v-if="scope.row.level === '1'" type="success"
+              >二级权限</el-tag
+            >
+            <el-tag v-if="scope.row.level === '2'" type="warning"
+              >三级权限</el-tag
+            >
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
   </div>
